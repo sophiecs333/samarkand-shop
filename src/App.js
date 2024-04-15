75213c17e5a56e18d8e62dceadf8502d34530e97
Sophie Sorin <sophie.sorin@hellofresh.com>
2024-04-15 08:25:39 +0200

    test: Commit test

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>Samarkand Shop</p>
+        <p>commit test</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
