160f3f25b06f9dd24495baeaa2fa6a926bb40d97
Sophie Sorin <sophie.sorin@hellofresh.com>
2024-04-23 14:41:26 +0200

    feat: Add base of the website

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import './Footer.css';
+
+export const Footer = () => {
+  return (
+    <div className="Footer">
+      <p>Footer</p>
+      <p>logo instagram</p>
+      <p>logo facebook</p>
+      <p>Copyright blabla</p>
+    </div>
+  );
+};
